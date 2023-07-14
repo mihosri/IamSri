e698417bbf09fe9b106cc53d4559ed99abe2f8b8
mihosri <srineethi.gurumurthigirahhalakshmi@edu.sait.ca>
2023-07-14 16:20:44 -0600

    Flip Button and Logo components added

--- frontend/src/components/BusinessCard/BusinessCard.jsx
+++ frontend/src/components/BusinessCard/BusinessCard.jsx
@@ -1,40 +1,38 @@
 import React from 'react';
 import './BusinessCard.css';
+import Logo from '../Logo/Logo.jsx';
+import FlipButton from '../FlipButton/FlipButton.jsx';
 
 function BusinessCard() {
   
-  function flipCard() {
-    var card = document.querySelector('.card');
-    if (card.style.transform === 'rotateY(180deg)') {
-      card.style.transform = 'rotateY(0)';
-    } else {
-      card.style.transform = 'rotateY(180deg)';
-    }
-  }
-
   return (
     <>
     <div className='container'>
       <div className='card'>
         
         <div className="front">
-          <h1>Sri Guru Girahha</h1>
-          <p>Aspiring full stack developer</p>
-          <a href='https://www.linkedin.com/in/sri-guru-girahha/' target='blank'>Sri LinkedIn</a>
 
-          <button onClick={()=>flipCard()}>Flip Card</button>
+          {/* Logo component */}
+          <Logo/>
+
         </div>
 
         <div className='back'>
-          <h1>Rear view</h1>
-          <a href='https://github.com/mihosri' target='blank'>Sri Github</a>
 
-          <button onClick={()=>flipCard()}>Flip Card</button>
+          {/* Logo component */}
+          <Logo/>
+          
         </div>
         
       </div>
     </div>
-   
+
+    <br></br>
+    <br></br>
+
+    {/* Flip Button */}
+    <FlipButton/>
+    
   </>
   );
 };
