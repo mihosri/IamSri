1bab840c86be4e936ce409b9edb4a00ba81d61c7
mihosri <srineethi.gurumurthigirahhalakshmi@edu.sait.ca>
2023-07-24 00:43:57 -0600

    changes in directory structure

---
+++ src/components/BackgroundText/BackgroundText.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './BackgroundText.css';
+
+const BackgroundText = () => {
+  return (
+    <>
+      <div className='textholder'>
+        <p className='bgtext'>
+          Ready to join your team
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default BackgroundText;
